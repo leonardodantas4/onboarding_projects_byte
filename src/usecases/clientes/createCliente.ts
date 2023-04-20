d7ffd631b29e536ebba23f1e03ed61d8ed556486
leonardodantas4 <leonardodds@hotmail.com>
2023-04-20 12:57:53 -0300

    Usecases Cliente #3

---
+++ src/usecases/clientes/createCliente.ts
@@ -0,0 +1,23 @@
+import { PrismaClient, Cliente } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export interface typeCliente{
+    nome: string,
+    cpf: string,
+    celular: string,
+    endereco: string
+}
+
+export class CriandoCliente{
+    async execute({nome,cpf,celular,endereco}:typeCliente): Promise<Cliente>{
+        const novoCliente = await prisma.cliente.create({
+            data:{
+                nome: nome,
+                cpf: cpf,
+                celular: celular,
+                endereco: endereco
+            }
+        })
+        return novoCliente;
+    }
+}
