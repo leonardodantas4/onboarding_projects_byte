a1db233677b459c3d4b77a4e752a8edc5a452abc
leonardodantas4 <leonardodds@hotmail.com>
2023-04-20 20:18:39 -0300

    Pequena alteração

--- src/controller/clienteController/updateCliente.ts
+++ src/controller/clienteController/updateCliente.ts
@@ -4,30 +4,27 @@ import { AtualizandoCliente } from "../../usecases/clientes/updateCliente";
 import { prisma } from "./createCliente";
 
 export class UpdateClienteController{
-    async handle(req: Request, res: Response){
-      const {nome, cpf, celular, endereco} = req.body;
-  
-      try{
-        const testeCliente = await prisma.cliente.findUnique({
-          where: {
-            cpf: cpf
-          }
-        })
-        if(testeCliente == null){
-          return res.status(StatusCodes.UNAUTHORIZED).send({error: "Esse cliente não existe"})
+  async handle(req: Request, res: Response){
+    const {nome, cpf, celular, endereco} = req.body;
+
+    try{
+      const testeCliente = await prisma.cliente.findUnique({
+        where: {
+          cpf: cpf
         }
-  
-        const update = new AtualizandoCliente();
-  
-        //Atualizando Cliente
-        const cliente = await update.execute({nome, cpf, celular, endereco});
-  
-        //Retornando Cliente Atualizado
-        return res.status(StatusCodes.CREATED).send({
-          cliente,
-        },);
-      } catch (err){
-        return res.status(StatusCodes.BAD_REQUEST).send({error: "Falha ao tentar atualizar cliente"});
+      })
+      if(testeCliente == null){
+        return res.status(StatusCodes.UNAUTHORIZED).send({error: "Esse cliente não existe"})
       }
+
+      const update = new AtualizandoCliente();
+      const cliente = await update.execute({nome, cpf, celular, endereco});
+
+      return res.status(StatusCodes.CREATED).send({
+        cliente,
+      },);
+    } catch (err){
+      return res.status(StatusCodes.BAD_REQUEST).send({error: "Falha ao tentar atualizar cliente"});
     }
   }
+}
