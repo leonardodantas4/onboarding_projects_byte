06ff47e9e21f43d8a4b4f231f010ab329ef553fd
leonardodantas4 <leonardodds@hotmail.com>
2023-04-20 19:35:07 -0300

    Rotas updateCliente #3

--- src/routes/cliente.routes.ts
+++ src/routes/cliente.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateClienteController } from "../controller/clienteController/createCliente";
+import { UpdateClienteController } from "../controller/clienteController/updateCliente"; 
 
 const clienteRoutes = Router();
 
 const createCliente = new CreateClienteController();
+const updateClienteController = new UpdateClienteController();
 
 clienteRoutes.post("/create", createCliente.handle);
+clienteRoutes.put("/update", updateClienteController.handle);
 
 export {clienteRoutes};
