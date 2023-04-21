4b7a364bb4551e4ae5b3b933296d6153bcd271d0
leonardodantas4 <leonardodds@hotmail.com>
2023-04-20 23:40:12 -0300

    Excluindo updateHamburguer e modificando createHamburguer #5

--- src/routes/hamburguer.routes.ts
+++ src/routes/hamburguer.routes.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
 import { CreateHamburguerController } from "../controller/hamburguerController/createHamburguer";
-import { UpdateHamburguerController } from "../controller/hamburguerController/updateHamburguer"; 
 
 const hamburguerRoutes = Router();
 
 const createHamburguer = new CreateHamburguerController();
-const updateHamburguerController = new UpdateHamburguerController();
 
 hamburguerRoutes.post("/create", createHamburguer.handle);
-hamburguerRoutes.put("/update", updateHamburguerController.handle);
 
 export {hamburguerRoutes};
