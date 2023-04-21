6cc787d31761924e213faab771be5259c43ca538
leonardodantas4 <leonardodds@hotmail.com>
2023-04-21 16:56:30 -0300

    Correção usecases createHamburguer #5

--- src/usecases/hamburgueres/createHamburguer.ts
+++ src/usecases/hamburgueres/createHamburguer.ts
@@ -1,5 +1,5 @@
-import { PrismaClient, Hamburguer } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient, Hamburguer } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export interface typeHamburguer{
     nome: string,
