89575a198ea952d5c6d65fa7d020880d8e9555cb
leonardodantas4 <leonardodds@hotmail.com>
2023-04-20 21:28:07 -0300

    Rotas updateHamburguer #5

--- src/routes/hamburguer.routes.ts
+++ src/routes/hamburguer.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateHamburguerController } from "../controller/hamburguerController/createHamburguer";
+import { UpdateHamburguerController } from "../controller/hamburguerController/updateHamburguer"; 
 
 const hamburguerRoutes = Router();
 
 const createHamburguer = new CreateHamburguerController();
+const updateHamburguerController = new UpdateHamburguerController();
 
 hamburguerRoutes.post("/create", createHamburguer.handle);
+hamburguerRoutes.put("/update", updateHamburguerController.handle);
 
 export {hamburguerRoutes};
