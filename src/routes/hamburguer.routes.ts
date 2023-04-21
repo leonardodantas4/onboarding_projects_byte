17a2c0c812276f7737af18b37c1c121e47b74c54
leonardodantas4 <leonardodds@hotmail.com>
2023-04-21 17:23:28 -0300

    Routes deleteHamburguer #5

--- src/routes/hamburguer.routes.ts
+++ src/routes/hamburguer.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { CreateHamburguerController } from "../controller/hamburguerController/createHamburguer";
-import { UpdateHamburguerController } from "../controller/hamburguerController/updateHamburguer"; 
+import { UpdateHamburguerController } from "../controller/hamburguerController/updateHamburguer";
+import { DeleteHamburguerController } from "../controller/hamburguerController/deleteHamburguer";
 
 const hamburguerRoutes = Router();
 
 const createHamburguer = new CreateHamburguerController();
 const updateHamburguerController = new UpdateHamburguerController();
+const deleteHamburguerController = new DeleteHamburguerController();
 
 hamburguerRoutes.post("/create", createHamburguer.handle);
 hamburguerRoutes.put("/update/:nome", updateHamburguerController.handle);
+hamburguerRoutes.delete("/delete/:nome", deleteHamburguerController.handle);
 
 export {hamburguerRoutes};
