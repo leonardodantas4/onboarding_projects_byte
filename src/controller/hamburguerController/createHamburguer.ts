8a3165e233d0e4d8cc246724b3d66e2ae550a19f
leonardodantas4 <leonardodds@hotmail.com>
2023-04-21 16:56:02 -0300

    Correção Controller createHamburguer #5

--- src/controller/hamburguerController/createHamburguer.ts
+++ src/controller/hamburguerController/createHamburguer.ts
@@ -1,29 +1,27 @@
-import { Hamburguer } from "@prisma/client";
-import { prisma } from "../clienteController/createCliente";
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { StatusCodes } from "http-status-codes";
+import { CriandoHamburguer } from "../../usecases/hamburgueres/createHamburguer";
 
-export interface typeHamburguer {
-    nome: string,
-    descricao: string,
-    ingredientes: string,
-    preco: number
-  }
-  
-  export class CreateHamburguerController {
-    handle(arg0: string, handle: any) {
-        throw new Error("Method not implemented.");
-    }
-    async execute({nome, descricao, ingredientes, preco}:typeHamburguer):Promise<Hamburguer>{
-      const newHamburguer = await prisma.hamburguer.create({
-        data: {
-          nome: nome,
-          descricao: descricao,
-          ingredientes: ingredientes,
-          preco: preco
+export const prisma = new PrismaClient;
+
+export class CreateHamburguerController{
+    async handle(req:Request, res: Response){
+        const {nome,descricao,preco,ingredientes} = req.body;
+        try{
+            if(await prisma.hamburguer.findUnique({where:{nome:nome}})){
+                return res.status(StatusCodes.UNAUTHORIZED).send({error:"Hamburguer já existente"})
+            }
+
+            const create = new CriandoHamburguer();
+            // Criando hamburguer
+            const hamburguer = await create.execute({nome,descricao,preco,ingredientes});
+
+            return res.send({
+                hamburguer,
+            },);
+        } catch(err){
+            return res.status(StatusCodes.NOT_FOUND).send({error:"Falha no registro de hamburguer"});
         }
-      })
-  
-      return newHamburguer;
-    }
-  }
-
-export { prisma };
+    };
+}
